5a614ffb4015dd21ed10491ed9e7a899181b4bef
srbbsl <saurabbsl@gmail.com>
2025-01-19 17:31:44 +0545

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,23 @@ import { useSelector } from "react-redux";
 export const Home = () => {
 
     const { blogs } = useSelector((state) => state.blogSlice); //((state) => state) return ko shortcut
-    console.log(blogs);
+    // console.log(blogs);
 
     return(
         <div>
             
+            {
+                blogs.map((blog) =>{
+                    return <div key={blog.id}>
+                        <h1>{blog.title}</h1>
+                        <p>{blog.detail}</p>
+                        <p>{blog.location}</p>
+                        <p>{blog.genres}</p>
+                        <p>{blog.country}</p>
+                    </div>
+                })
+            }
+
         </div>
     )
 };

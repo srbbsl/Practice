88efef4034048ba3311aa109f7aab985e7ebd4ff
srbbsl <saurabbsl@gmail.com>
2025-01-20 19:40:46 +0545

    changes

--- src/pages/AddForm.jsx
+++ src/pages/AddForm.jsx
@@ -1,25 +1,62 @@
-import { Button, Input } from "@material-tailwind/react";
+import { Button, Input, Textarea } from "@material-tailwind/react";
+import { Formik } from "formik";
 
 
 
 export const AddForm = () => {
     return (
       <div className="max-w-[400px] p-5">
-        <form className="space-y-4">
-          <div>
-            <Input 
-              label="title"
-              onChange={(e) => {
-                console.log(e.target.value);
-              }}
-            />
-          </div>
 
+        <Formik
+          initialValues={{
+            title: '',
+            detail: '',
+          }}
+
+          onSubmit={(val) => {
+            console.log(val)
+          }}
+        >
+
+         
+        
+          {({ handleChange, handleSubmit, values, errors, setFieldValue }) => (
+            <form onSubmit={handleSubmit} className="space-y-4">
+                  <div>
+                    <Input 
+                      label="title"
+                      name='title'
+                      value={values.title}
+                      onChange={handleChange}
+                      // onChange={(e) => {
+                      //   console.log(e.target.value);
+                      // }}
+                    />
+                  </div>
+
+                  <div>
+                    <Textarea 
+                      label="detail"
+                      name='detail' 
+                      value={values.detail}
+                      onChange={handleChange}
+                    />
+                  </div>
+
+                  
+                    <Button type='submit'>Submit</Button>
+                  
+                  
+            </form>
+          )
+        }
           
-            <Button>Submit</Button>
-          
-          
-        </form>
+            
+       
+            
+
+        </Formik>
+        
       </div>
     )
 };

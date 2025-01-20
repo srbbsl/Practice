283ba4e57642cf94df7f02967e7e2a38362280cd
srbbsl <saurabbsl@gmail.com>
2025-01-20 14:34:43 +0545

    changes

--- src/index.js
+++ src/index.js
@@ -2,25 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { App } from './App';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import { ToastContainer } from 'react-toastify';
+
 
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
+    <div>
 
-    <Provider store={store}>
         <App />
 
-        <ToastContainer 
-            position='top-center'
-            pauseOnHover={false}
-            autoClose={1000}
-        />
-    </Provider>
+    </div>
+    
+        
+
+        
+    
     
 
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,8 @@
-import { useSelector } from "react-redux";
-
 
 
 export const Home = () => {
-
-    const { blogs } = useSelector((state) => state.blogSlice); //((state) => state) return ko shortcut
-    // console.log(blogs);
-
-    return(
+    return (
         <div>
-            
-            {
-                blogs.map((blog) =>{
-                    return <div key={blog.id}>
-                        <h1>{blog.title}</h1>
-                        <p>{blog.detail}</p>
-                        <p>{blog.location}</p>
-                        <p>{blog.genres}</p>
-                        <p>{blog.country}</p>
-                    </div>
-                })
-            }
 
         </div>
     )

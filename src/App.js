1b435a74ca4819a4ff224f81fd1e1e58f0cf7e27
srbbsl <saurabbsl@gmail.com>
2025-01-21 12:07:01 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router"
 import { RouterProvider } from "react-router-dom";
-import { RootLayout } from "./Component/RootLayout";
+
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { CategoryItems } from "./pages/CategoryItems";
 import { ItemDetail } from "./pages/ItemDetail";
 import { FormHandle } from "./pages/FormHandle";
+import { RootLayout } from "./Component/RootLayout";
 // import { ListWithAvatar } from "./Component/ListWithAvatar";
 
 

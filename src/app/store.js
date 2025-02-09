15f465790a947fd509461fa9aaa3e0c90bf6bb72
srbbsl <saurabbsl@gmail.com>
2025-02-09 12:47:29 +0545

    feb 4.2 finished

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authApi } from "../features/auth/authApi";
 
 
 
 export const store = configureStore({
     reducer: {
-        
+        [authApi.reducerPath]: authApi.reducer
     },
     middleware: (getDefaultMIddleware) => getDefaultMIddleware()
     .concat([
-
+        authApi.middleware
     ])
 });

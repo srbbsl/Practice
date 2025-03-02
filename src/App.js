e15b55474aff76cf04d204326f24bad7b4d18215
srbbsl <saurabbsl@gmail.com>
2025-03-02 12:49:51 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
 import React from 'react'
-import { createBrowserRouter } from 'react-router'
-import { Login } from './features/auth/Login'
-import { RouterProvider } from 'react-router-dom'
-import { RootLayout } from './ui/RootLayout'
-import { Register } from './features/auth/Register'
 
 export const App = () => {
-
-    const router = createBrowserRouter([
-        {
-            path: '/',
-            element: <RootLayout />,
-            children: [
-                {
-                    index: true,
-                    element: <Login />
-                },
-                {
-                    path: 'register',
-                    element: <Register />
-                },
-            ]
-        }
-    ])
-  return < RouterProvider router={router} />
+  return (
+    <div>App</div>
+  )
 }
--- src/ui/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Header = () => {
-  return (
-    <header className='bg-black py-3 px-6 text-white'>
-        <h1 className='text-2xl font-bold'>Shopify</h1>
-    </header>
-  )
-}

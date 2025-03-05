63ea7f7db15e84f52728c493df2fc2233a318985
srbbsl <saurabbsl@gmail.com>
2025-03-05 17:47:43 +0545

    feb 24.2 finished

--- src/features/utils/validator.js
+++ src/features/utils/validator.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
-export const validCategory = ['electronics', 'jewelery', 'clothing\'s'];
+
 
 export const LoginSchema = () => Yup.object({
     email: Yup.string().email('Invalid Email').required('Email is required'),
@@ -13,9 +13,17 @@ export const registerSchema = () => Yup.object({
     password: Yup.string().required('Password is required'),
 });
 
+export const validCategory = ['electronics', 'jewelery', 'clothing\'s'];
+
+export const validImageType = ['image/jpg', 'image/jpeg', 'image/png', 'image/svg', 'image/webp', 'image/avif'];
+
 export const productSchema = () => Yup.object({
     title: Yup.string().required('Title is required'),
     description: Yup.string().required('Description is required'),
     price: Yup.number().required('Price is required'),
     category: Yup.string().oneOf(validCategory, 'Choose valid category').required('Category is required'),
+    image: Yup.mixed().test((val) => {
+        // console.log(val);
+        return val && validImageType.includes(val?.type);
+    }).required('Image is required'),
 });

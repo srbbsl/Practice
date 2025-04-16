3eaae78889c8e97c57a9f8e12675503642ffac7f
srbbsl <saurabbsl@gmail.com>
2025-04-16 16:18:56 +0545

    changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { BlogCard } from '../Components/BlogCard';
 
 export const Home = () => {
     const { blogs } = useSelector((state) => state.blogSlice);
-    console.log(blogs)
+    // console.log(blogs)
 
     
   return (
-    <div>Home</div>
+    <div className='p-4 grid grid-cols-4 gap-3'>
+      {
+        blogs.map((blog) => {
+          return <BlogCard blog={blog} key={blog.id}/>
+         
+        })
+      }
+    </div>
   )
 }
